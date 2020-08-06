a226e5f382051ffd748327ad9d7457866c668846
cristhian <acristhian35@gmail.com>
2020-08-06 18:18:31 -0400

    add appointments controller and telescope lib

---
+++ app/Http/Controllers/Appointment/AppointmentController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Appointment;
+
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+use App\Appointment;
+
+class AppointmentController extends ApiController
+{
+  public function index()
+  {
+    $t = Appointment::query()->first();
+    $query = Appointment::query();
+    $query = $this->filterData($query, $t);
+    $datos = $query->get();
+      return $this->showAll($datos,200);
+  }
+
+  public function store(Request $request)
+  {
+        $request->validate([
+        'appoin_description' => 'required',
+    ]);
+   $datos = new Appointment($request->all());
+   $datos->save();
+    return response()->json('Cargo se agrego con exito!');
+  }
+
+  public function show($id)
+  {
+    //$dato = Appointment::find( $id);
+    $dato = Appointment::where( 'appoin_id', '=', $id)
+                              ->first();
+    return $this->showOne($dato,200);
+  }
+
+  public function update(Request $request, $id)
+  {
+    $dato = Appointment::findOrFail( $id);
+          $request->validate([
+            'appoin_description' => 'required',
+          ]);
+          $dato->appoin_description = $request->appoin_description;
+          $dato->save();
+          return response()->json([
+              'message' => 'Cargo se actualizo con exito!',
+              'dato' => $dato
+          ]);
+      }
+
+      public function destroy($id)
+      {
+          $dato = Appointment::findOrFail($id);
+          $dato->delete();
+          return response()->json([
+              'message' => 'Cargo se elimino con exito!'
+          ]);
+        }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//rutas de Cargos
+Route::get('appointments/', 'Appointment\AppointmentController@index');
+Route::get('appointments/{id}', 'Appointment\AppointmentController@show');
+Route::get('appointments/search/{req}', 'Appointment\AppointmentController@search');
+Route::post('appointments/', 'Appointment\AppointmentController@store');
+Route::put('appointments/{id}','Appointment\AppointmentController@update');
+Route::delete('appointments/{id}','Appointment\AppointmentController@destroy');
